db1c457f4979bd0ddc0a6c627d3bf4a3756a7e61
Teimur <t.bagirov2000@gmail.com>
2025-03-06 10:40:10 +0300

    дописал publication service

---
+++ publication-service/src/main/kotlin/org/bagirov/publicationservice/dto/request/PublicationUpdateRequest.kt
@@ -0,0 +1,15 @@
+package org.bagirov.publicationservice.dto.request.update
+
+import java.math.BigDecimal
+import java.util.*
+
+
+data class PublicationUpdateRequest(
+    val id: UUID,
+    val index: String?,
+    val title: String?,
+    val description: String?,
+    val author: String?,
+    val typeName: String?,
+    val price: BigDecimal?
+)
---
+++ publication-service/src/main/kotlin/org/bagirov/publicationservice/dto/response/PublicationResponse.kt
@@ -0,0 +1,14 @@
+package org.bagirov.publicationservice.dto.response
+
+import java.math.BigDecimal
+import java.util.*
+
+data class PublicationResponse (
+    val id: UUID,
+    val index: String,
+    val title: String,
+    val author: String,
+    val description: String?,
+    val publicationType: String,
+    val price: BigDecimal
+)
