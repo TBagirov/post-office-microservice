7967c68d1b82672d3d08cc8d59a5fa4d75375068
Teimur <t.bagirov2000@gmail.com>
2025-03-19 23:09:41 +0300

    подправил генерацию отчетов, теперь все работает

--- auth-service/src/main/kotlin/org/bagirov/authservice/dto/UserUpdatedEventDto.kt
+++ auth-service/src/main/kotlin/org/bagirov/authservice/dto/UserUpdatedEventDto.kt
@@ -1,22 +1,36 @@
 package org.bagirov.authservice.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 import java.util.*
 
+/**
+ * DTO для события обновления пользователя.
+ */
+@Schema(description = "DTO с информацией о событии обновления пользователя")
 data class UserUpdatedEventDto(
+
+    @Schema(description = "ID пользователя", example = "550e8400-e29b-41d4-a716-446655440000")
     val userId: UUID,
 
+    @Schema(description = "Имя пользователя", example = "Иван")
     val name: String,
 
+    @Schema(description = "Фамилия пользователя", example = "Иванов")
     val surname: String,
 
+    @Schema(description = "Отчество пользователя", example = "Иванович")
     val patronymic: String,
 
+    @Schema(description = "Email пользователя", example = "ivanov@example.com")
     val email: String,
 
+    @Schema(description = "Телефон пользователя", example = "+7 999 123-45-67")
     val phone: String,
 
+    @Schema(description = "Дата последнего обновления в формате HH:mm, dd/MM/yyyy", example = "14:30, 10/03/2024")
     val updatedAt: LocalDateTime?,
 
+    @Schema(description = "Роль пользователя", example = "SUBSCRIBER")
     val role: String
 )
--- report-service/src/main/kotlin/org/bagirov/reportservice/repository/PublicationRepository.kt
+++ report-service/src/main/kotlin/org/bagirov/reportservice/repository/PublicationRepository.kt
@@ -1,6 +1,5 @@
 package org.bagirov.reportservice.repository
 
-import org.bagirov.reportservice.dto.response.ReportPublicationResponse
 import org.bagirov.reportservice.entity.PublicationEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
@@ -12,7 +11,8 @@ interface PublicationRepository : JpaRepository<PublicationEntity, UUID> {
     fun findByIndex(index: String): PublicationEntity?
     fun findByPublicationId(publicationId: UUID): PublicationEntity?
 
-    @Query(nativeQuery = true, value = """
+    @Query(
+        nativeQuery = true, value = """
         SELECT 
             publication_id, 
             index, 
@@ -22,6 +22,7 @@ interface PublicationRepository : JpaRepository<PublicationEntity, UUID> {
             price, 
             count_subscriber 
         FROM report_publication
-    """)
+    """
+    )
     fun getReportPublications(): List<Any>
 }
